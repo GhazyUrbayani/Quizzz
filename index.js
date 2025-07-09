c0e94f6b3ec72f52b17fb025671ad84d2d2df28a
GhazyUrbayani <163674292+GhazyUrbayani@users.noreply.github.com>
2025-07-09 22:22:44 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import Login from './Login.js';  // Tambahkan ekstensi .js
-import Quiz from './Quiz.js';    // Tambahkan ekstensi .js
-import Result from './Result.js';  // Tambahkan ekstensi .js
-import './App.css';  // Pastikan file CSS diimpor dengan benar
+import ReactDOM from 'react-dom/client';
+import Login from './Login.js';
+import Quiz from './Quiz.js';
+import Result from './Result.js';
+import './App.css';
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
@@ -17,4 +17,9 @@ const App = () => (
   </Router>
 );
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
