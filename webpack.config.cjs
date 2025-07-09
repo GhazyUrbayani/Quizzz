38c3264b2c7833bf4672067c3635ca21ceee55d7
GhazyUrbayani <163674292+GhazyUrbayani@users.noreply.github.com>
2025-07-09 22:34:09 +0700

    Update webpack.config.cjs

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -4,9 +4,9 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: './index.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js',
-    publicPath: '/',
+    clean: true,
   },
   mode: 'production',
   resolve: {
